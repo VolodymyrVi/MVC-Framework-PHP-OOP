0dcf43e0bb24bcc4161d3394bfdbb60c957054de
VolodymyrVi <sqlphpjs@gmail.com>
2021-07-05 11:24:40 +0300

    added method index() and about()

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,6 +4,15 @@ class Pages
 {
     public function __construct()
     {
-        echo "Loader";
+
+    }
+
+    public function index()
+    {
+        echo "Home page";
+    }
+    public function about()
+    {
+        echo "About";
     }
 }
