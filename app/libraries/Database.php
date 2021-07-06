3cddbb18a948c51dbb7f639ab367eea1bfac3f7f
VolodymyrVi <sqlphpjs@gmail.com>
2021-07-06 16:39:53 +0300

    fixed

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -12,7 +12,7 @@ class Database
 
     public function __construct()
     {
-        $conn = 'mysql:host=' . $this->dbHost . ';dbName=' . $this->dbName;
+        $conn = 'mysql:host=' . $this->dbHost . ';dbname=' . $this->dbName;
         $options = array(
             PDO::ATTR_PERSISTENT => true,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
