ccc1d1883b16ed4482bff8a99c815758427452a4
VolodymyrVi <sqlphpjs@gmail.com>
2021-07-06 09:59:54 +0300

    added config

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc_frame');
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('URLROOT', 'localhost/mvcframework');
+
+define('SITENAME', 'MVC Framework');
