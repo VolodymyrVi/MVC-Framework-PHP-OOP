f6f311099a28ad06ec489ad5fa58428275cf0643
VolodymyrVi <sqlphpjs@gmail.com>
2021-07-06 16:38:48 +0300

    update config file

--- app/config/config.php
+++ app/config/config.php
@@ -3,10 +3,10 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'mvc_frame');
+define('DB_NAME', 'mvcframework');
 
 define('APPROOT', dirname(dirname(__FILE__)));
 
-define('URLROOT', 'localhost/mvcframework');
+define('URLROOT', 'localhost/mvcframework/');
 
 define('SITENAME', 'MVC Framework');
