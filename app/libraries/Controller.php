76d02fd88a4d6da66b5936d7ad28e599a680b5da
VolodymyrVi <sqlphpjs@gmail.com>
2021-07-06 09:05:33 +0300

    added doc

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -2,13 +2,25 @@
 
 class Controller
 {
+    /**
+     *  function model()
+     *
+     * @param string $model
+     * @return void
+     */
     public function model($model)
     {
         require_once'../app/models' . $model . '.php';
 
         return new $model();
     }
-
+    /**
+     * function view()
+     *
+     * @param string $view
+     * @param array $data
+     * @return void
+     */
     public function view($view, $data = [])
     {
         if(file_exists('../app/views/' . $view . '.php')){
